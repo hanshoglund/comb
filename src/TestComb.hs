26125fde144ae37995899f81c2799265727b75cb
Hans Höglund <hans@hanshoglund.se>
2013-08-25 00:46:36 +0200

    Pa and sndfile tests

--- src/TestComb.hs
+++ src/TestComb.hs
@@ -1,4 +1,6 @@
 
+{-# LANGUAGE MultiParamTypeClasses #-}
+
 module Main where
 
 import Data.Int
@@ -9,29 +11,44 @@ import Foreign.Storable
 import Sound.PortAudio
 import Control.Concurrent (threadDelay)
 
+import qualified Sound.File.Sndfile as Sndfile
+
+
+-- Bogus instances
+instance Sndfile.Buffer [] Double where
+    fromForeignPtr p i j = return []
+    toForeignPtr _ = return (error "No function: toForeignPtr")
+                    
 
 main = do                  
-    -- putStrLn "Starting up"
-    -- putStrLn "Using default stream..."
+    putStrLn "Reading sound file test.wav"
+    (info, bogusBuffer) <- Sndfile.readFile "test.wav"
+    let _ = (bogusBuffer :: Maybe [Double])
+    putStrLn $ "  Sample rate: " ++ (show $ Sndfile.samplerate info)
+    putStrLn $ "  Number of channels: " ++ (show $ Sndfile.channels info)
+
+    putStrLn "Starting up PortAudio"
+    putStrLn "  Using default stream..."
     initialize
     stream <- 
         openDefaultStream 1 2 44100 (Just 64) 
             (Just $ callback) 
-            (Just $ putStrLn "Finished")
+            (Just $ putStrLn "  Finished")
 
     case stream of 
         Left e  -> do
-            putStrLn ("Error: " ++ show e)
+            putStrLn ("  Error: " ++ show e)
             terminate
             return ()
         Right s -> do
-            putStrLn "Starting stream"
+            putStrLn "  Starting stream"
             startStream s
             threadDelay 150000000
             -- stopStream s
             -- threadDelay 1000000
             -- terminate
             return ()
+            putStrLn "  Stream has finished"
     return ()
     
 callback info flags count inp outp = do
