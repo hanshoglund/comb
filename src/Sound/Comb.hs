e7bcd917e0d33bde3e1cb886a2a0b2e7b6030999
Hans Höglund <hans@hanshoglund.se>
2013-09-10 00:38:07 +0200

    Starts basic impl

--- src/Sound/Comb.hs
+++ src/Sound/Comb.hs
@@ -1,17 +1,25 @@
 
-module Sound.Comb (
+{-# LANGUAGE TypeOperators, DeriveFunctor, GADTs #-}
 
+module Sound.Comb (
+    Sink,
+    Source,
+    Task,
+    run,
 
 
     ) where
 
 
 import Data.Int
+import Data.Functor.Contravariant
 import Foreign.Ptr
 import Foreign.C.Types
+import Control.Applicative
 import Control.Monad
 import Foreign.Storable
 import Sound.PortAudio
 import Control.Concurrent (threadDelay)
 
+-- TODO use non-default audio device
 
