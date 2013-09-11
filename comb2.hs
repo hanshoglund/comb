1a077a0df0e0cba2eadb31925fbd7b8aaf77745d
Hans Höglund <hans@hanshoglund.se>
2013-09-11 23:40:39 +0200

    Cleaning

--- comb2.hs
+++ comb2.hs
@@ -186,79 +186,6 @@ run = go . simplify
         go (Input n) s      = (s, stateInputs s !! n)
         go (Output n a) s   = first id $ a `run` s -- TODO modify s
 
-{-
-
-
-newtype Signal = Signal { getSignal ::
-    State -> (State, Double)
-    }
-instance Num Signal where
-    (+) = lift2 (+)
-    (*) = lift2 (*)
-    negate        = (* (-1))
-    abs           = lift abs
-    signum        = lift signum
-    fromInteger x = signal (fromInteger x)
-instance Fractional Signal where
-    recip = lift recip
-    fromRational x = signal (fromRational x)
-
-time    :: Signal
-input   :: Int -> Signal
-signal  :: Double -> Signal
-lift    :: (Double -> Double) -> Signal -> Signal
-lift2   :: (Double -> Double -> Double) -> Signal -> Signal -> Signal
-loop    :: (Double -> Double -> (Double,Double)) -> Signal -> Signal
-delay   :: Signal -> Signal
-time        = Signal $ \s -> (s, fromIntegral (stateCount s) / stateRate s)
-input n     = Signal $ \s -> (s, stateInputs s !! n)
-signal x    = Signal $ \s -> (s, x)
-lift f a    = Signal $ \s -> let 
-    (sa, xa) = (getSignal a) s 
-    in (sa, f xa)
-lift2 f a b = Signal $ \s -> let
-    (sa, xa) = (getSignal a) s
-    (sb, xb) = (getSignal b) sa
-    in (sb, f xa xb)
-    
-loop f a = Signal $ \s -> let
-    (sa, xa) = (getSignal a) s
-    -- xb       = readBuf s
-    xb       = 0
-    (xc,xd)  = f xb xa
-    -- sc       = writeBuf xc sa
-    sc = sa
-    in (sc, xd)
-    
-delay       = loop $ \o n -> (n, o)
-
-runSignal :: Signal -> [[Double]] -> [Double]
-runSignal a inputs = snd $ mapAccumL proc def inputs
-    where
-        proc s as = let 
-            s2 = s { 
-                stateInputs = as
-            }
-            (s3, x) = (getSignal a) s2
-            s4 = s3 {
-                stateCount = stateCount s2 + 1
-            }
-            in (s4, x)
-
-putSignal :: Signal -> [[Double]] -> IO ()
-putSignal a xs = mapM_ (putStrLn.toBars) $ runSignal a xs
-
-main :: IO ()
-main = putSignal sig inp
-    where    
-        -- one channel input
-        inp = (fmap.fmap) (/ 10) $ concat $ replicate 3 $ transpose [[-10..10]]
-                                        
-        sig = time*0.5
-        -- sig = lift2 (\a b -> a) (input 0) (input 0)
-        -- sig = (delay.delay.delay.delay.delay) (input 0)
-
-
 
 -- | From range (0,1) to range (-1,1)
 toFull :: Num a => a -> a
@@ -268,14 +195,15 @@ toFull x = (x*2)-1
 toPos  :: Fractional a => a -> a
 toPos x  = (x+1)/2
 
+
 -- | View as bars if in range (-1,1)
 toBars :: RealFrac a => a -> String
 toBars x = let n = round (toPos x * width) in
-    if n > width || n < 0
+    if n > width || n < 0
         then replicate (width+1) ' ' ++ "|"
         else replicate n ' ' ++ "." ++ replicate (width-n) ' ' ++ "|"
-    where
-        width = 80               -}
+    where 
+        width = 80
 
 
 
@@ -285,17 +213,6 @@ second f (a,b)      = (a, f b)
 cast'               = fromJust . cast
 fromJust (Just x)   = x
 dup x               = (x, x)
-
-
--- | Compose stepper functions.
--- 
--- You should only use @steps f g@ when @f (g x) = x@.
---
--- > steps pred succ n a = toEnum (fromEnum a + n)
-steps :: (a -> a) -> (a -> a) -> Int -> a -> a
-steps neg pos n 
-    | n <  0 = foldr (.) id (replicate (negate n) neg)
-    | n >= 0 = foldr (.) id (replicate n pos)
 
 
 
