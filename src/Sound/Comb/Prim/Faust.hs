bdc9f1d9402cf013821490239e4c7149e996ec9c
Hans Höglund <hans@hanshoglund.se>
2013-09-17 20:04:48 +0200

    Faust export

--- src/Sound/Comb/Prim/Faust.hs
+++ src/Sound/Comb/Prim/Faust.hs
@@ -30,10 +30,24 @@ signalToFaust a = prefix ++ "process = " ++ go 0 a ++ ";\n\n"
         func ['(',o1,o2,')'] = [o1,o2]
         func a               = a
         
-        prefix = "time_ = (+(1) ~ _); \n\n"
+        prefix = ""
+            ++ "declare name \"test\";\n"
+            ++ "\n"
+            ++ "util_ = environment {\n"
+            ++ "    filter = library(\"filter.lib\");\n"
+            ++ "    music = library(\"music.lib\");\n"
+            ++ "    math = library(\"math.lib\");\n"
+            ++ "};"
+            ++ "\n"
+            ++ "random_             =   +(12345) ~ *(1103515245);"
+            ++ "integrate_(f)       =   +(f) ~ _;"
+            ++ "samples_            =   integrate_(1)-1;"
+            ++ "time_               =   float(samples_) / float(util_.math.SR);"
+            ++ "\n"
 
         -- Lift  String (Double -> Double) Signal                  -- string is optional name
         -- Lift2 String (Double -> Double -> Double) Signal Signal -- string is optional name
+
 
 
 drawSignalFaust :: Signal -> IO ()
@@ -42,3 +56,21 @@ drawSignalFaust a = do
     system "faust2svg test.dsp"
     -- system "open test-svg/process.svg"
     return ()
+
+
+writeSignal :: FilePath -> Signal -> IO ()
+writeSignal path a = do   
+    drawSignalFaust a -- DEBUG
+    
+    writeFile "test.dsp" (signalToFaust a)
+
+    -- TODO assure dir and input file
+    system "faust -a sndfile.cpp test.dsp > test-sndfile/test.cpp"
+    
+    system "c++ -lsndfile -O3 test-sndfile/test.cpp -o test-sndfile/test"
+    system $ "test-sndfile/test test-sndfile/in.wav " ++ "./" ++ path
+
+    system $ "open " ++ path -- DEBUG
+    return ()
+
+
